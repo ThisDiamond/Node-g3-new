bc30285664f3eae424e4cac0951fa67498303a06
DilshodbekNodeJs <DilshodbekNodejs@gmail.com>
2022-12-27 13:04:22 +0500

    m

--- 2-oy/10-dars/main.js
+++ 2-oy/10-dars/main.js
@@ -1,1 +1,130 @@
 // 1 //
+// let k = 15;
+// let n = 3;
+
+// for (let i = 0; i < 3; i = i + 1) {
+//   console.log(k);
+// }
+
+// 2 //
+// let a = 15;
+// let b = 25;
+// for (; a <= b; a++) {
+//   console.log(a);
+// }
+
+// 3 //
+// let a = 15;
+// let b = 25;
+// for (let i = b - 1; a < i; i--) {
+//   console.log(i);
+// }
+
+// 4 //
+// let price = 4.7;
+
+// for (let i = 0; i <= 10; i = i + 1) {
+//     console.log(price*i);
+// }
+
+// 5 //
+// let price = 50_000; // nollarni _ bolish mumkin qiymati ozgarmaydi
+
+// for (let i = 0.1; i < 1; i = i + 0.1) {
+//   console.log(price*i.toFixed(1));
+//   // .toFixed() Kasrlarni aniqroq qiymatini olish uchun
+// }
+
+// 6 //
+// let price = 3.5;
+
+// for (let i = 0; i <= 2; i = i + 0.2) {
+//     console.log(price*i);
+// }
+
+// 7 //
+// let a = 4;
+// let b = 7;
+// let y = 0;
+
+// for (; a <= b; a++) {
+//   console.log(a);
+//   y = y + a;
+// }
+// console.log(y);
+
+// 8 //
+// let a = 4;
+// let b = 7;
+// let y = 1;
+
+// for (; a <= b; a++) {
+//   console.log(a);
+//   y = y * a;
+// }
+// console.log(y);
+
+// 10 //
+// let n = 5;
+// let s = 0;
+
+// for (let i = 1; i <= n; i++) {
+//   console.log(i);
+//   console.log(1 / i);
+//   console.log(s + 1 / i);
+//   s = s + 1 / i;
+// }
+// console.log(s.toFixed(1));
+
+// 11 //
+// let n = 5;
+// let s = 0;
+// for (let i = 0; i <= n; i++) {
+//   s = (n + i) ** 2;
+//   console.log(s);
+// }
+
+// 12 //
+// let n = 3;
+// let s = 1;
+// for (let i = 1; i <= n; i++) {
+//   s = s * (i + 0.1);
+//   console.log(s);
+// }
+
+// 13 // ????????????????????????
+// let n = 3;
+// let s = 1;
+
+// let ii = 1;
+
+// for (let i = 1; i <= n; i = i + 0.1) {
+//   s = ii * i;
+//   console.log((i * ii).toFixed(1));
+//   ii *= -1;
+// }
+
+// 14 //
+// let n = 5;
+// let s = 0;
+// for (let i = 1; i < n; i++) {
+//     s=s+(2*n-1);
+//     console.log(s);
+// }
+
+// 15 //
+// let n = 5;
+// let a = 0.5;
+// for (let i = 1; i <= n; i++) {
+//   a = a * n;
+//   a;
+// }
+
+// 16 //
+// let n = 5;
+// let a = 0.5;
+// let sum = 0;
+// for (let i = 1; i <= n; i++) {
+//     sum=a**i
+//     sum;
+// }
